66073f1e24abc6da57bb6fccad920b9e76e99d0e
notAxion <mr.bhakat456@gmail.com>
2023-12-02 18:32:39 +0530

    day2 part1

---
+++ 2023/day2/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.ReadFile("input")
+
+	fmt.Println(part1(file))
+}
+
+func part1(file []byte) int {
+	redMax, greenMax, blueMax := 12, 13, 14
+	total := 0
+
+	// each line
+line:
+	for i := 0; i < len(file); {
+		gameRecord := make([]byte, 0, 9)
+		for ; i < len(file); i++ {
+			if file[i] == ':' {
+				i++
+				break
+			}
+			gameRecord = append(gameRecord, file[i])
+		}
+		gameID := 0
+		fmt.Sscanf(string(gameRecord), "Game %d", &gameID)
+
+		// each game
+		for ; i < len(file); i++ {
+			whichCube := make([]byte, 0, 10)
+			for ; i < len(file) && !(file[i] == ';' || file[i] == ',' || file[i] == '\n'); i++ {
+				whichCube = append(whichCube, file[i])
+			}
+
+			reds, greens, blues := 0, 0, 0
+			cubes := 0
+			cubeColor := ""
+			fmt.Sscanf(string(whichCube), " %d %s", &cubes, &cubeColor)
+
+			switch cubeColor {
+			case "red":
+				reds = cubes
+			case "green":
+				greens = cubes
+			case "blue":
+				blues = cubes
+			default:
+				panic("what the color - " + cubeColor)
+			}
+
+			if reds > redMax || greens > greenMax || blues > blueMax {
+				for ; i < len(file); i++ {
+					if file[i] == '\n' {
+						i++
+						break
+					}
+				}
+				continue line
+			}
+			if i >= len(file) || file[i] == '\n' {
+				i++
+				break
+			}
+		}
+
+		total += gameID
+	}
+
+	return total
+}
