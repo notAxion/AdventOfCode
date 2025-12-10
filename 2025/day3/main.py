e6500c2595dce53907c2cba5819f207c28b1ecb4
notAxion <mr.bhakat456@gmail.com>
2025-12-10 19:39:00 +0530

    day3 part2

--- 2025/day3/main.py
+++ 2025/day3/main.py
@@ -25,4 +25,31 @@ def part1():
     print(total)
 
 
+def part2():
+    total = 0
+
+    file = open("input", encoding="utf-8")
+    for line in file.readlines():
+        line = line.strip()
+        line_total = 0
+        # print(line)
+
+        last_index = -1
+        for digit in range(12):
+            digit_num = 0
+            for i in range(last_index + 1, len(line) - (11 - digit)):
+                # print(f"{num},", end="")
+                num = int(line[i])
+                if num > digit_num:
+                    digit_num = num
+                    last_index = i
+
+            line_total = (line_total * 10) + digit_num
+
+        total += line_total
+
+    print(total)
+
+
 part1()
+part2()
