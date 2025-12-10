7df99c7d937d6e8725ad65e1b63731c7da1573e5
notAxion <mr.bhakat456@gmail.com>
2025-12-10 14:14:12 +0530

    day1 part2

--- 2025/day1/main.py
+++ 2025/day1/main.py
@@ -1,5 +1,5 @@
-def part1():
-    with open("input", encoding="utf-8") as file:
+def part1(filepath: str):
+    with open(filepath, encoding="utf-8") as file:
         password = 0
         dial = 50
         for line in file.readlines():
@@ -16,7 +16,43 @@ def part1():
                     dial += num
             if dial % 100 == 0:
                 password += 1
+
         print(password)
 
 
-part1()
+def part2(filepath: str):
+    with open(filepath, encoding="utf-8") as file:
+        password = 0
+        dial = 50
+        for line in file.readlines():
+            # print(f"line: {line.strip()}")
+            line = line.strip()
+            char = line[0]
+            # print(char)
+            num = int(line[1:])
+
+            olddial = dial
+            match char:
+                case "L":
+                    dial -= num
+
+                    actual_dial = (-1 * olddial) % 100
+                    x_dial = actual_dial + num
+                    if x_dial >= 100:
+                        hunds = x_dial // 100
+                        password += hunds
+
+                case "R":
+                    dial += num
+
+                    actual_dial = olddial % 100
+                    x_dial = actual_dial + num
+                    if x_dial >= 100:
+                        hunds = x_dial // 100
+                        password += hunds
+
+        print(password)
+
+
+part1("input")
+part2("input")
